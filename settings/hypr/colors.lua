abef5f6318c42f95ecd8e7755ee5c6c757f91f57
max <emirshn000@gmail.com>
2026-06-24 10:57:56 +0300

    Add files via upload

---
+++ settings/hypr/colors.lua
@@ -0,0 +1,104 @@
+return {
+    image = "/home/max/Pictures/Alice/Fixed/wallhaven-d85k3o_1920x1080.png",
+
+    background = "0xff111318",
+
+    error = "0xffffb4ab",
+
+    error_container = "0xff93000a",
+
+    inverse_on_surface = "0xff2e3035",
+
+    inverse_primary = "0xff3f5f90",
+
+    inverse_surface = "0xffe1e2e9",
+
+    on_background = "0xffe1e2e9",
+
+    on_error = "0xff690005",
+
+    on_error_container = "0xffffdad6",
+
+    on_primary = "0xff06305f",
+
+    on_primary_container = "0xffd5e3ff",
+
+    on_primary_fixed = "0xff001b3c",
+
+    on_primary_fixed_variant = "0xff254777",
+
+    on_secondary = "0xff273141",
+
+    on_secondary_container = "0xffd9e3f8",
+
+    on_secondary_fixed = "0xff121c2b",
+
+    on_secondary_fixed_variant = "0xff3e4758",
+
+    on_surface = "0xffe1e2e9",
+
+    on_surface_variant = "0xffc4c6cf",
+
+    on_tertiary = "0xff3e2845",
+
+    on_tertiary_container = "0xfff8d8fe",
+
+    on_tertiary_fixed = "0xff28132f",
+
+    on_tertiary_fixed_variant = "0xff563e5d",
+
+    outline = "0xff8e9099",
+
+    outline_variant = "0xff43474e",
+
+    primary = "0xffa8c8ff",
+
+    primary_container = "0xff254777",
+
+    primary_fixed = "0xffd5e3ff",
+
+    primary_fixed_dim = "0xffa8c8ff",
+
+    scrim = "0xff000000",
+
+    secondary = "0xffbdc7dc",
+
+    secondary_container = "0xff3e4758",
+
+    secondary_fixed = "0xffd9e3f8",
+
+    secondary_fixed_dim = "0xffbdc7dc",
+
+    shadow = "0xff000000",
+
+    source_color = "0xff047be8",
+
+    surface = "0xff111318",
+
+    surface_bright = "0xff37393e",
+
+    surface_container = "0xff1d2024",
+
+    surface_container_high = "0xff282a2f",
+
+    surface_container_highest = "0xff33353a",
+
+    surface_container_low = "0xff191c20",
+
+    surface_container_lowest = "0xff0c0e13",
+
+    surface_dim = "0xff111318",
+
+    surface_tint = "0xffa8c8ff",
+
+    surface_variant = "0xff43474e",
+
+    tertiary = "0xffdbbce1",
+
+    tertiary_container = "0xff563e5d",
+
+    tertiary_fixed = "0xfff8d8fe",
+
+    tertiary_fixed_dim = "0xffdbbce1",
+
+}
